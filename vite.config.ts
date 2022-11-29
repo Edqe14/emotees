b60b54fb3121570e589dfc161187a0ee175c1833
Edqe_ <34704796+Edqe14@users.noreply.github.com>
2022-11-29 13:26:36 +0700

    feat: add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,51 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()]
+  plugins: [
+    tsconfigPaths(),
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Emotees',
+        short_name: 'Emotees',
+        orientation: 'portrait',
+        theme_color: '#f8fafc',
+        background_color: '#f8fafc',
+        icons: [
+          {
+            src: '/assets/icons/manifest-icon-192.maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '/assets/icons/manifest-icon-192.maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: '/assets/icons/manifest-icon-512.maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '/assets/icons/manifest-icon-512.maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      },
+      workbox: {
+        clientsClaim: true,
+      }
+    })
+  ]
 });
